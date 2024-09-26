1a3f58f0de3ebe0d47917d509214862b97a492c6
gyumingim <a316205045@gmail.com>
2024-09-26 16:54:16 +0900

    init

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\a3162\flask\myenv
+set VIRTUAL_ENV=C:\Users\Software\flask\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
